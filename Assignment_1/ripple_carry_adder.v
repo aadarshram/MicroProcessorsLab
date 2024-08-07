0aaea5efedef650596464de6af98665484f51815
Aadarsh R <ramachandranaadarsh@gmail.com>
2024-08-07 13:15:35 +0530

    fix ripple carry adder

--- Assignment_1/ripple_carry_adder.v
+++ Assignment_1/ripple_carry_adder.v
@@ -1,12 +1,17 @@
 // This code describes a model for 4-bit ripple carry adder using x4 full adders
-module ripple_carry_adder(i11, i12, i13, i14, i21, i22, i23, i24, cin, o1, o2, o3, o4, cout);
-input i11, i12, i13, i14, i21, i22, i23, i24, cin;
+// module ripple_carry_adder(i11, i12, i13, i14, i21, i22, i23, i24, cin, o1, o2, o3, o4, cout);
+module ripple_carry_adder(i1, i2, cin, o, cout);
+// input i11, i12, i13, i14, i21, i22, i23, i24, cin;
+input [3:0] i1, i2;
+input cin;
 wire c1, c2, c3;
-output o1, o2, o3, o4, cout;
+// output o1, o2, o3, o4
+output[3:0] o;
+output cout;
 
-full_adder fa1(i11, i21, c0, o1, c1);
-full_adder fa2(i12, i22, c1, o2, c2);
-full_adder fa3(i13, i23, c2, o3, c3);
-full_adder fa4(i14, i24, c3, o4, cout);
+full_adder fa1(i1[0], i2[0], cin, o[0], c1);
+full_adder fa2(i1[1], i2[1], c1, o[1], c2);
+full_adder fa3(i1[2], i2[2], c2, o[2], c3);
+full_adder fa4(i1[3], i2[3], c3, o[3], cout);
 
 endmodule
--- Assignment_1/tb_ripple_carry_adder.v
+++ Assignment_1/tb_ripple_carry_adder.v
@@ -1,51 +1,31 @@
 // This code describes a testbench for testing and simulating the ripple carry 4-bit adder circuit model
 module tb_ripple_carry_adder;
-reg i11, i12, i13, i14, i21, i22, i23, i24, cin;
-wire o1, o2, o3, o4, cout;
-ripple_carry_adder rca(i11, i12, i13, i14, i21, i22, i23, i24, cin, o1, o2, o3, o4, cout);
+reg[3:0] i1, i2;
+reg cin;
+wire[3:0] o;
+wire cout;
+ripple_carry_adder rca(i1, i2, cin, o, cout);
 
 initial 
 begin
     cin = 1'b0;
-    i11 = 1'b0;
-    i12 = 1'b0;
-    i13 = 1'b0;
-    i14 = 1'b0;
-    i21 = 1'b0;
-    i22 = 1'b0;
-    i23 = 1'b0;
-    i24 = 1'b0;
+    i1 = 4'b0000;
+    i2 = 4'b0000;
     #20 
-    i11 = 1'b0;
-    i12 = 1'b1;
-    i13 = 1'b0;
-    i14 = 1'b1;
-    i21 = 1'b0;
-    i22 = 1'b0;
-    i23 = 1'b1;
-    i24 = 1'b1;
+    i1 = 4'b0010;
+    i2 = 4'b0101;
     #20 
-    i11 = 1'b1;
-    i12 = 1'b1;
-    i13 = 1'b0;
-    i14 = 1'b1;
-    i21 = 1'b0;
-    i22 = 1'b1;
-    i23 = 1'b0;
-    i24 = 1'b1;
+    i1 = 4'b0111;
+    i2 = 4'b1000;
     #20 
-    i11 = 1'b1;
-    i12 = 1'b0;
-    i13 = 1'b1;
-    i14 = 1'b1;
-    i21 = 1'b1;
-    i22 = 1'b0;
-    i23 = 1'b1;
-    i24 = 1'b0;
+    i1 = 4'b0110;
+    i2 = 4'b1000;
 
     # 30 $finish;
 
 end
-initial $monitor($time, " i14 = %b, i13 = %b, i12 = %b, i11 = %b, i24 = %b, i23 = %b, i22 = %b, i21 = %b, cin = %b, cout = %b, o4 = %b, o3 = %b, o2 = %b, o1 = %b", i14, i13, i12, i11, i24, i23, i22, i21, cin, cout, o4, o3, o2, o1);
+// initial $monitor($time, " i14 = %b, i13 = %b, i12 = %b, i11 = %b, i24 = %b, i23 = %b, i22 = %b, i21 = %b, cin = %b, cout = %b, o4 = %b, o3 = %b, o2 = %b, o1 = %b", i14, i13, i12, i11, i24, i23, i22, i21, cin, cout, o4, o3, o2, o1);
+initial $monitor($time, " i1 = %b, i2 = %b, cin = %b, cout = %b, o = %b ", i1, i2, cin, cout, o);
+
 endmodule
     
