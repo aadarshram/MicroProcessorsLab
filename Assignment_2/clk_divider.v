ba0cc352be79ef1902ab84d43d04db69292d9fb9
Aadarsh R <ramachandranaadarsh@gmail.com>
2024-08-16 00:04:38 +0530

    Make more modular

--- Assignment_2/clk_divider.v
+++ Assignment_2/clk_divider.v
@@ -3,6 +3,7 @@ module clk_divider(outClk, inClk, reset);
 input inClk;
 input reset;
 output reg outClk;
+//reg clockCount;
 reg [25:0] clockCount;
 
 always@(negedge reset or posedge inClk)
@@ -10,12 +11,15 @@ begin
     if (reset == 1'b0)
         begin
             outClk = 1'b0;
+            //clockCount = 1'd0;
             clockCount = 26'd0;
         end
     else
         begin
+        // if (clockCount >= 1'd1)
         if (clockCount >= 26'd50000000)
             begin
+                // clockCount = 1'd0;
                 clockCount = 26'd0;
                 outClk = ~ outClk;
             end
