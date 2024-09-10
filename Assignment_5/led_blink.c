f2c2edbe65da283d6f4365bd8330c84a613303e3
Aadarsh Ramachandiran <142728380+aadarshram@users.noreply.github.com>
2024-09-10 14:42:12 +0530

    Update led_blink.c

--- Assignment_5/led_blink.c
+++ Assignment_5/led_blink.c
@@ -2,11 +2,11 @@
 #include <util/delay.h>
 int main(void)
 {
-	DDRC = 0x01;
+	DDRD = 0x01;
 	while(1){
-		PORTC = 0x01;
+		PORTD = 0x01;
 		_delay_ms(1000);
-		PORTC = 0x00;
+		PORTD = 0x00;
 		_delay_ms(1000);
 	}
 }
