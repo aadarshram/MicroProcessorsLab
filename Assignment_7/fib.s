eebbb607d908fc544516d41980dc3fbfc0a3145a
Aadarsh Ramachandiran <142728380+aadarshram@users.noreply.github.com>
2024-09-25 15:32:56 +0530

    Update fib.s

--- Assignment_7/fib.s
+++ Assignment_7/fib.s
@@ -1,11 +1,11 @@
 ; Program to find 10th fibonacci number 
-AREA Reset, CODE, READONLY
-    
-    ENTRY
-        LDR r0, =0 ; Initialized to fib(0). Holds fib(n-2)
-        LDR r1, =1 ; Initialized to fib(1). Holds fib(n-1)
-        LDR r3, =0 ; Initialized to 0. Holds fib(n)
-        LDR r2, =10 ; Counter initialized to 10 since we need 10th fib number
+    AREA Reset, CODE, READONLY
+        
+        ENTRY
+            LDR r0, =0 ; Initialized to fib(0). Holds fib(n-2)
+            LDR r1, =1 ; Initialized to fib(1). Holds fib(n-1)
+            LDR r3, =0 ; Initialized to 0. Holds fib(n)
+            LDR r2, =10 ; Counter initialized to 10 since we need 10th fib number
 LOOP
     ADD r3, r1, r0 ; Fib sequence. fib(n) = fib(n-1) + fib(n-2)
     MOV r0, r1 ; Update fib(n-2)
@@ -13,5 +13,6 @@ LOOP
     SUB r2, r2, #1 ; Dec counter
     CMP r2, 2 ;  Check if counter has reached 2. 
     BNE LOOP ;  Loop until 10th number is reached
-
+STOP
+    B STOP
     END
