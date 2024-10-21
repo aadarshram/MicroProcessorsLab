4a43056384f3075e2b1050e31cbf58169d90d695
Aadarsh Ramachandiran <142728380+aadarshram@users.noreply.github.com>
2024-10-21 07:23:51 +0530

    Update ripple_carry_adder.v

--- Assignment_1/modules/ripple_carry_adder.v
+++ Assignment_1/modules/ripple_carry_adder.v
@@ -1,11 +1,8 @@
 // This code describes a model for 4-bit ripple carry adder using x4 full adders
-// module ripple_carry_adder(i11, i12, i13, i14, i21, i22, i23, i24, cin, o1, o2, o3, o4, cout);
 module ripple_carry_adder(i1, i2, cin, o, cout);
-// input i11, i12, i13, i14, i21, i22, i23, i24, cin;
 input [3:0] i1, i2;
 input cin;
 wire c1, c2, c3;
-// output o1, o2, o3, o4
 output[3:0] o;
 output cout;
 
