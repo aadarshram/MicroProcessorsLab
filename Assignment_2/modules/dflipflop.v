95ca236d8f24ba47f93e5834415dee316dcec228
Aadarsh Ramachandiran <142728380+aadarshram@users.noreply.github.com>
2024-10-21 08:18:13 +0530

    Update dflipflop.v

--- Assignment_2/modules/dflipflop.v
+++ Assignment_2/modules/dflipflop.v
@@ -1,11 +1,10 @@
 // This code describes a behavioural model for D flip flop
 module dflipflop(q, qbar, d, clk);
-output q, qbar;
+output reg q, qbar;
 input d, clk;
-reg q, qbar;
 always@(posedge clk)
     begin 
-        q = d;
-        qbar = ~d;
+        q <= d;
+        qbar <= ~d;
     end
 endmodule
